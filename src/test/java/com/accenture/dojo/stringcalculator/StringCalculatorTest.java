0b328a4dffdb4d16aadc769007d9a43faf589d51
CHIRON Freddy Ext DTSI/DSI <HVLH5852@orange.com>
2018-01-10 15:16:31 +0100

    [TECH] update dependencies version

--- src/test/java/com/accenture/dojo/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/accenture/dojo/stringcalculator/StringCalculatorTest.java
@@ -1,18 +1,19 @@
-import static org.assertj.core.api.Assertions.assertThat;
+package com.accenture.dojo.stringcalculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
-    private StringCalculator sc = new StringCalculator();
-
-    @Test
-    public void check_add(){
-        // Test
-        int result = sc.add("1");
+	@Test
+	public void check_setup_ready() {
+		StringCalculator stringCalculator = new StringCalculator();
+		
+		// Test
+        int result = stringCalculator.add("1");
 
         // Assertions
         assertThat(result).isEqualTo(-1);
-    }
+	}
 
 }
